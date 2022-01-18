7f956fcbb3736a4b143eadaef0e6cc25318d4ff3
dannyjenkins38 <dannyjenkins38@outlook.com>
2022-01-18 20:35:13 +0000

    extra line on line 45

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -42,7 +42,7 @@ public class TestGreeter {
   } 
      @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "World, Thanks you making me Happy and consistant";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
