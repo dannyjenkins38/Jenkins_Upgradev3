ef8c64b40d4c985529381ada6dde4fcffb0cda7e
edwinkwesi <amooedwin2@gmail.com>
2022-01-26 15:11:47 +0000

    change greeting phrase function module

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -34,18 +34,18 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
+  @Test
+  public void greetShouldIncludeGreetingPhrase() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
     @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   } 
-
-  @Test
-  public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World, Thanks you making me Happy";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  } 
-    
+      
 }
