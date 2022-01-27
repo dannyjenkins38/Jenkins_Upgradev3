f4e91aa37715343e1155141793e23cfdc9a98580
edwinkwesi <amooedwin2@gmail.com>
2022-01-27 14:38:20 +0000

    added a text line to TestGreeter.java line 40

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,7 +37,7 @@ public class TestGreeter {
   
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy every day";
+    String someone = "World, Thanks you making me Happy every day and its awesome";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   } 
